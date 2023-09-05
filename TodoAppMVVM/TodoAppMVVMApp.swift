cf88525e16e794f2d359ed5b9f6f64b57cf0cb8b
fuadreza <fuadreza1011@gmail.com>
2023-09-05 17:54:36 +0700

    Initial Commit

---
+++ TodoAppMVVM/TodoAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppMVVMApp.swift
+//  TodoAppMVVM
+//
+//  Created by Fuad Reza Pahlevi on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoAppMVVMUITests/TodoAppMVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TodoAppMVVMUITestsLaunchTests.swift
+//  TodoAppMVVMUITests
+//
+//  Created by Fuad Reza Pahlevi on 05/09/23.
+//
+
+import XCTest
+
+final class TodoAppMVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
